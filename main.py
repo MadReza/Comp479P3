d2dc51f15d3e85da848e12f8cbed1c70e45e71b6
Reza Madabadi <rezmaza@gmail.com>
2016-12-02 15:10:23 -0500

    Added function to retrieve all txt files from each Directory

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 print "Main App"
 
 import os
+from glob import glob
 
 scraped_root_directory = "www_roots"
 
 #Get All Departments:
 departments = [x[1] for x in os.walk(scraped_root_directory)][0]
 
+def get_all_txt_files_in(path):
+    """Returns all text files in all folders and sub-folders found"""
+    return [y for x in os.walk(path) for y in glob(os.path.join(x[0], '*.txt'))]
+
 if __name__ == '__main__':
     #for later redirects.
-    print departments
+    for x in departments:
+        print "Department: ", x
+        path = scraped_root_directory + "/" + x
+        for y in get_all_txt_files_in(path):
+            print "\t", y
