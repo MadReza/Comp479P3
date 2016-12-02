f8c8fda3233665a75a0331dff1010ddedac24663
Reza Madabadi <rezmaza@gmail.com>
2016-12-02 15:57:32 -0500

    Added scorer per department and per file

--- main.py
+++ main.py
@@ -12,10 +12,28 @@ def get_all_txt_files_in(path):
     """Returns all text files in all folders and sub-folders found"""
     return [y for x in os.walk(path) for y in glob(os.path.join(x[0], '*.txt'))]
 
+def get_score(data):
+    """Get score for file"""
+    #TODO: remove link from scorer ? if required ?
+    return len(data)#TODO, return score for that content
+
+def get_text_from(file_path):
+    """Retrieve text from file"""
+    with open(file_path, 'r') as file:
+        return file.read()
+
+def get_department_score(department):
+    """Gets the total score for that department"""
+    path = scraped_root_directory + "/" + department
+    score = 0
+
+    for file_path in get_all_txt_files_in(path):
+        file_content = get_text_from(file_path)
+        score = score + get_score(file_content)
+    return score
+
 if __name__ == '__main__':
-    #for later redirects.
+    #TODO: redirects and option calls
     for x in departments:
-        print "Department: ", x
-        path = scraped_root_directory + "/" + x
-        for y in get_all_txt_files_in(path):
-            print "\t", y
+        print "Department:", x
+        print "\tScore:", get_department_score(x)
