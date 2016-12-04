05991adf1611a7ab8dd3bd1b6472757061b28ad5
Matt Rauch <Mat@Matts-MacBook-Pro.local>
2016-12-04 17:17:26 -0500

    added soft-cap /per dir for the crawler (2MB), crawled some more data for testing

--- P3/spiders/Spider.py
+++ P3/spiders/Spider.py
@@ -2,12 +2,15 @@ import scrapy
 from scrapy.selector import Selector
 import os
 import os.path
+import sys
 
 class MySpider(scrapy.Spider):
     name = "base_spider"
     base_dir = "../www_roots"
-    cur_root_dirname = ""
     cur_root_url = ""
+
+    folder_size_dict = {}
+    Max_Folder_Size = 1048576*2 #1MB*2 = 2MB
 
     def start_requests(self):
         urls = [
@@ -28,38 +31,35 @@ class MySpider(scrapy.Spider):
             os.makedirs(self.base_dir)
 
         for url in urls:
-            #TODO: Replicated this into a function so you can call this for possible new urls
             # www.example.com/foo/bar
             self.cur_root_url = url.split('://')[1]
             self.cur_root_url = url.split('.html')[0]
             print (self.cur_root_url)
 
+            # e.g. Biology (used to keep record in size dictionary)
+            department = self.cur_root_url.split('/')[-1]
             # www_root/bar
-            self.cur_root_dirname = self.base_dir + "/" + self.cur_root_url.split('/')[-1]
+            cur_root_dirname = self.base_dir + "/" + department
+
+            # init departments folder size
+            print ("DICT STAT")
+            self.folder_size_dict[department] = sum(os.path.getsize(cur_root_dirname + '/' + f) for f in os.listdir(cur_root_dirname) if os.path.isfile(cur_root_dirname + '/' + f))
+            print (self.folder_size_dict)
 
             # create dir for the current sub-root page
-            if not os.path.exists(self.cur_root_dirname):
-                os.makedirs(self.cur_root_dirname)
+            if not os.path.exists(cur_root_dirname):
+                os.makedirs(cur_root_dirname)
 
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def do_things_for_url(self, url):
-        """TODO: rename this maybe ??? :P """
-        # www.example.com/foo/bar
-        self.cur_root_url = url.split('://')[1]
-        self.cur_root_url = url.split('.html')[0]
-        print (self.cur_root_url)
-
-        # www_root/bar
-        self.cur_root_dirname = self.base_dir + "/" + self.cur_root_url.split('/')[-1]
-
-        # create dir for the current sub-root page
-        if not os.path.exists(self.cur_root_dirname):
-            os.makedirs(self.cur_root_dirname)
-
-        yield scrapy.Request(url=url, callback=self.parse)
+            if self.folder_size_dict[department] < self.Max_Folder_Size:
+                request = scrapy.Request(url=url, callback=self.parse)
+                request.meta['cur_root_dirname'] = cur_root_dirname
+                request.meta['department'] = department
+                yield request
 
     def parse(self, response):
+        cur_root_dirname = response.meta['cur_root_dirname']
+        department = response.meta['department']
+
         # example.html
         page = response.url.split("/")[-1]
         if '.' in page:
@@ -73,27 +73,32 @@ class MySpider(scrapy.Spider):
 
             # handle file saving and append count if filename exists
             count = 1
-            if not os.path.isfile(self.cur_root_dirname + '/' + page):
-                with open(self.cur_root_dirname + '/' + filename + ".txt", 'a') as f:
-                    site = get_tags(response)
-                    f.write(site.encode('utf-8', 'ignore'))
-                self.log('Saved file %s' % filename)
-            else:
-                while os.path.isfile(self.cur_root_dirname + '/' + filename + str(count) + ".txt"):
-                    count += 1
-                with open(self.cur_root_dirname + '/' + filename + str(count) + ".txt", 'a') as f:
-                    site = get_tags(response)
-                    f.write(site.encode('utf-8', 'ignore'))
-                self.log('Saved file %s' % page)
+            if not os.path.isfile(cur_root_dirname + '/' + page):
 
-            # follow url
-            for href in response.css('a::attr(href)').extract():
-                # if starts with slash and not moving up dirs
-                utf = str(href).encode('utf-8', 'ignore')
-                if utf.startswith('/') and ".." not in utf:
-                    # COULD CHECK IF FILE EXISTS BEFORE EVEN SENDING REQUEST?, SAVE BANDWIDTH
-                    yield scrapy.Request(response.urljoin(href),
-                                         callback=self.parse)
+                write_to_file(self, response, cur_root_dirname, department, filename)
+            else:
+                while os.path.isfile(cur_root_dirname + '/' + filename + str(count) + ".txt"):
+                    count += 1
+                write_to_file(self, response, cur_root_dirname, department, filename)
+
+            # follow url if current dept folder size is under (1MB) ?
+            if self.folder_size_dict[department] < self.Max_Folder_Size:
+                for href in response.css('a::attr(href)').extract():
+                    # if starts with slash and not moving up dirs
+                    utf = str(href).encode('utf-8', 'ignore')
+                    if utf.startswith('/') and ".." not in utf:
+                        request = scrapy.Request(response.urljoin(href), callback=self.parse)
+                        request.meta['cur_root_dirname'] = cur_root_dirname
+                        request.meta['department'] = department
+                        yield request
+
+
+def write_to_file(self, response, cur_root_dirname, department, filename):
+    with open(cur_root_dirname + '/' + filename + ".txt", 'a') as f:
+        site = get_tags(response)
+        f.write(site.encode('utf-8', 'ignore'))
+    self.log('Saved file %s' % filename)
+    self.folder_size_dict[department] += sys.getsizeof(site)
 
 
 def get_tags(response):
